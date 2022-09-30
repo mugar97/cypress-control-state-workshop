c6643059feef4d4e245b5052909b2f14b2f48e09
Cristhian Murcia <mugar97@gmail.com>
2022-09-30 12:47:10 -0500

    Focusing on Purchase feature

--- demoblaze/cypress/e2e/cart.cy.js
+++ demoblaze/cypress/e2e/cart.cy.js
@@ -9,15 +9,3 @@ describe('Cart', () => {
         cy.get("#tbodyid").should("be.visible"); 
     });
 });
-
-// TODO list:
-// Products list:
-//  - renders proper information when there are products
-//  - shows an empty list when there are no products
-//  - total shows right total (experiment with calculations)
-//  - User actions:
-//    - deletes a product
-//    - Place Order button opens a modal with the right total
-//    - Dismiss Place Order modal
-//  - Place Order from:
-//    - Alerts - empty name or credit card
